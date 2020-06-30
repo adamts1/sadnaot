9fa2dd029564be0f4d83dbb1bc6f1441076d635e
adamtsityat@gmail.com <Adam1234!>
2020-06-30 14:15:33 +0300

    send email

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import smtplib, ssl
 
 app = Flask(__name__)
-
-posts = {
-    0: {'post_id': 1,
-        'title': "Heyyy",
-        'content': "You"
-    }
-}
-
 
 @app.route('/')
 def home():
@@ -19,11 +11,29 @@ def home():
 @app.route('/test', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')
-        result = request.form
-        print(result)
-    return render_template('test.jinja2', result = result)
+        # result = request.form
+        application_name = request.form.get('name')
+        application_subject = request.form.get('subject')
+        application_message = request.form.get('message')
+        application_email = request.form.get('email')
+        subject = "פניה מאתר סדנאות"
+        TEXT = " שם: "+application_name+"\n"+" נושא: "+application_subject+"\n"+" פרטי הודעה: "+application_message+"\n"+" דואר אלקטרוני: "+application_email
+        message = 'Subject: {}\n\n{}'.format(subject, TEXT).encode('utf-8')
+        port = 587  # For starttls
+        smtp_server = "smtp.gmail.com"
+        sender_email = "kryonps@gmail.com"
+        receiver_email = "kryonps@gmail.com"
+        password = "Kryon123!"
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.ehlo()  # Can be omitted
+            server.starttls(context=context)
+            server.ehlo()  # Can be omitted
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
+        return render_template('home.jinja2')
+
 
 
 if __name__ == '__main__':
--- send_email.py
+++ send_email.py
@@ -1,5 +1,19 @@
-from flask import Flask
-from flask_mail import Mail
+import smtplib, ssl
 
-app = Flask(__name__)
-mail = Mail(app)
+port = 587  # For starttls
+smtp_server = "smtp.gmail.com"
+sender_email = "kryonps@gmail.com"
+receiver_email = "kryonps@gmail.com"
+password = "Kryon123!"
+
+SUBJECT = "subject"
+TEXT = "text"
+message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
+
+context = ssl.create_default_context()
+with smtplib.SMTP(smtp_server, port) as server:
+    server.ehlo()  # Can be omitted
+    server.starttls(context=context)
+    server.ehlo()  # Can be omitted
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
